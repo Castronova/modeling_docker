23c177ec3a842150ed81996855027cdeb9f97897
Tony Castronova <Castronova.anthony@gmail.com>
2016-03-09 14:46:30 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python
 
-echo Hello World
+import os
+import requests
 
-printenv
+response_url = os.environ['RESPONSE_URL']
+abort_url = os.environ['ABORT_URL']
 
+print response_url
+print abort_url
+
+r = requests.post(response_url, result_text='some result')
