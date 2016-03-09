00cc98ba03a34d9edd030c3c988207759ee5a8f4
Tony Castronova <Castronova.anthony@gmail.com>
2016-03-08 22:36:21 -0500

    Update pandoc.sh
    changed run script

--- pandoc.sh
+++ pandoc.sh
@@ -1,17 +1,10 @@
-#!/bin/sh
+#!/usr/bin/python
 
+import requests
+import os
+import json
 
-echo $INPUT_URL
-echo $INPUT_FORMAT
-echo $OUTPUT_FORMAT
-echo $RESPONSE_URL
-echo $ABORT_URL
-
-mkdir -p /tmp
-curl "$INPUT_URL" > /tmp/input.$INPUT_FORMAT
-pandoc -f $INPUT_FORMAT -t $OUTPUT_FORMAT -o /tmp/output.$OUTPUT_FORMAT /tmp/input.$INPUT_FORMAT
-curl --form "fileupload=@/tmp/output.$OUTPUT_FORMAT" "$RESPONSE_URL"
-
-
-
-
+requests.post(os.environ['RESPONSE_URL'], data={ 
+    'result_text': "result text",
+    'result_data': json.dumps(os.environ)
+})
